69c9b9d685d5b7c37e4155823870da586a0d386b
bookazzzz <goorujke@yandex.ru>
2022-01-05 22:19:52 +0300

    model card

---
+++ models/card.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: [2, "Минимальная длина поля 'name' - 2 символа."],
+    maxlength: [30, "Максимальная длина поля 'name' - 30 символов."],
+  },
+  link: {
+    type: String,
+    required: [true, "Поле 'link' должно быть заполнено."],
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    ref: "user",
+  },
+  likes: [{
+    type: mongoose.ObjectId,
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }, });
+
+module.exports = mongoose.model("card", cardSchema);
