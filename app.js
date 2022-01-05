0dd22c95aa3517461f6e6aef8ced490cc15950b9
bookazzzz <goorujke@yandex.ru>
2022-01-05 23:11:22 +0300

    create controllers cards

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const mongoose = require("mongoose");
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
+
 
 mongoose.connect("mongodb://localhost:27017/mestodb", {
   useUnifiedTopology: true,
@@ -15,5 +23,5 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
 
 
 app.listen(PORT, () => {
-  console.log(`Актуальная ссылка на сервак: http://localhost:${PORT}`);
+  console.log(`Актуальная ссылка на сервер: http://localhost:${PORT}`);
 });
